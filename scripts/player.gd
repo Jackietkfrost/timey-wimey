a9ea868644bb9bbbb13b2da503951c8252cdc02e
Wolfy Hige <castielhige@gmail.com>
2025-07-18 17:54:44 -0400

    Rewind and Pause rework
    Added rewind function and reworked how pausing object work

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,9 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
+
+signal entered_game(game_ref:Node2D)	
+
+var gameInstance:Node2D
+var rewind:bool = false
 
 @export var speed:float = 100
 @export var gravity:float = 30
@@ -7,6 +12,12 @@ extends CharacterBody2D
 @export var jump_force:float = 400
 
 @onready var viewport:Viewport = get_viewport()
+
+
+func _on_entered_game(game_ref: Node2D) -> void:
+	gameInstance = game_ref
+	print(gameInstance)
+	
 
 func _physics_process(_delta):
 	
@@ -33,12 +44,21 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _input(event:InputEvent):
+	
 	if event.is_action_pressed("Time"):
 		var shockwave:ShaderMaterial = $Camera2D/CanvasLayer/ColorRect.material
 		var screenspace_player_pos = viewport.get_canvas_transform() * self.position \
 		/ Vector2(viewport.size)
 		shockwave.set_shader_parameter("center", screenspace_player_pos)
 		$Camera2D/CanvasLayer/AnimationPlayer.play("shockwave")
-		get_tree().paused = true
-		await get_tree().create_timer(2,true, false, true).timeout
-		get_tree().paused = false
+		gameInstance.timeshift.emit("Pause")
+
+	if event.is_action_pressed("Rewind"):
+		if gameInstance:
+			print(gameInstance)
+			var shockwave:ShaderMaterial = $Camera2D/CanvasLayer/ColorRect.material
+			var screenspace_player_pos = viewport.get_canvas_transform() * self.position \
+			/ Vector2(viewport.size)
+			shockwave.set_shader_parameter("center", screenspace_player_pos)
+			$Camera2D/CanvasLayer/AnimationPlayer.play("shockwave")
+			gameInstance.timeshift.emit("Rewind")
