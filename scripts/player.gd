11d6ff775312efa9ec09c820d03dd14dda6269f1
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-07-17 23:21:02 -0400

    Update player.gd
    Added static typing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-@export var speed = 100
-@export var gravity = 30
-@export var max_horizontal_speed = 100
-@export var max_fall_speed = 200
-@export var jump_force = 400
+@export var speed:float = 100
+@export var gravity:float = 30
+@export var max_horizontal_speed:float = 100
+@export var max_fall_speed:float = 200
+@export var jump_force:float = 400
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -15,7 +15,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump"):
 		velocity.y = -jump_force
 	
-	var horizontal_direction = Input.get_axis("Move Left", "Move Right")
+	var horizontal_direction:float = Input.get_axis("Move Left", "Move Right")
 	velocity.x = speed * horizontal_direction
 	if(horizontal_direction > 0):
 		$Body.flip_h = false
