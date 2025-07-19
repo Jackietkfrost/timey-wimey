a3a060034e5916eca45399253dfc2bee11260cb7
WizoSavage <47043063+WizoSavage@users.noreply.github.com>
2025-07-18 22:58:35 -0400

    last commit with changes

--- Objects/key.gd
+++ Objects/key.gd
@@ -1,45 +1,27 @@
 extends Area2D
 
-@onready var interact_label: Label = $Label
-@export var interact_name: String = ""
-@export var is_interactable: bool = true
-var current_interactions:= []
-var can_interact:= true
+@onready var interactable: Area2D = $Interactable
+var isInteractable: bool = false
 
-func _input(event:InputEvent):
-	if event.is_action_pressed("Interact") and can_interact:
-		if current_interactions:
-			can_interact = false
-			interact_label.hide()
-			
-			await current_interactions[0].interact.call()
-			
-			can_interact = true
-
-func _process(_delta: float) -> void:
-	if current_interactions and can_interact:
-		current_interactions.sort_custom(_sort_by_nearest)
-		if current_interactions[0].is_interactable:
-			interact_label.text = current_interactions[0].interact_name
-			interact_label.show()
-		else :
-			interact_label.hide()
-
-func _sort_by_nearest(area1, area2):
-	var area1_dist = global_position.distance_to(area1.global_position)
-	var area2_dist = global_position.distance_to(area2.global_position)
-	return area1_dist < area2_dist
-
-var interact:Callable = func():
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact") && isInteractable:
+		self.queue_free()
 		pass
+#func _ready() -> void:
+	#interactable.interact = _on_interact
+
+func _on_interact():
+	interactable.is_interactable = false
+	print("key picked up")
+
 
 func _on_body_entered(body: Node2D) -> void:
-	
-	current_interactions.push_back(body)
-	#queue_free()
-
+	if body is Player:
+		isInteractable = true
+	pass # Replace with function body.
 
 
 func _on_body_exited(body: Node2D) -> void:
-	current_interactions.erase(body)
-	
+	if body is Player:
+		isInteractable = false
+	pass # Replace with function body.
