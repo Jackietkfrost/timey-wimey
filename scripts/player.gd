5e79417b94ccbb13c940d6b7052eab6843c00bc1
WizoSavage <47043063+WizoSavage@users.noreply.github.com>
2025-07-18 20:11:41 -0400

    Update player.gd
    Added key variable and changed locations of items

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,6 @@ class_name Player extends CharacterBody2D
 
 signal entered_game(game_ref: Node2D)
 
-var gameInstance: Node2D
-var rewind: bool = false
-
 @export var speed: float = 100
 @export var gravity: float = 30
 @export var max_horizontal_speed: float = 100
@@ -15,13 +12,16 @@ var rewind: bool = false
 
 @onready var viewport: Viewport = get_viewport()
 
+var hasKey:bool = false
+var gameInstance: Node2D
+var rewind: bool = false
 
 func _on_entered_game(game_ref: Node2D) -> void:
 	gameInstance = game_ref
 	print(gameInstance)
 	
-
 func _physics_process(_delta):
+	
 	if !is_on_floor():
 		velocity.y += gravity
 		if velocity.y > max_fall_speed:
@@ -62,6 +62,9 @@ func _input(event: InputEvent):
 			shockwave.set_shader_parameter("center", screenspace_player_pos)
 			$Camera2D/CanvasLayer/AnimationPlayer.play("shockwave")
 			gameInstance.timeshift.emit("Rewind")
-
-		shockwave.set_shader_parameter("center", screenspace_player_pos)
-		$Camera2D/CanvasLayer/AnimationPlayer.play("shockwave-end")
+			shockwave.set_shader_parameter("center", screenspace_player_pos)
+			$Camera2D/CanvasLayer/AnimationPlayer.play("shockwave-end")
+		
+	if event.is_action_pressed("Interact"):
+		
+		pass
