456beeb7e7f769e8d3990484e198b506b5a046ee
WizoSavage <47043063+WizoSavage@users.noreply.github.com>
2025-07-19 03:09:31 -0400

    Door Functionality
    -door now takes you from initial level to level 2
    -added comments on how to add next level, as level 2 ends with printing the level name

--- Objects/door.gd
+++ Objects/door.gd
@@ -2,11 +2,18 @@ extends Area2D
 
 var isInteractable: bool = false
 var player : Player
+var doorOpen : bool = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Interact") && isInteractable && player:
-		if player.hasKey:
+	if Input.is_action_just_pressed("Interact") && isInteractable && player :
+		if player.hasKey && !doorOpen:
+			doorOpen = true
 			$DoorSprite.play("door_open")
+		elif doorOpen:
+			if get_tree().get_current_scene().get_name() == "Game":
+				get_tree().change_scene_to_file("res://assets/level_2.tscn")
+			else: #^COPY THAT, REPLACE if's   == "[INSERT_NEXTLEVEL_NAME]" do that below here 
+				print(get_tree().get_current_scene().get_name())
 		pass
 
 func _on_body_entered(body: Node2D) -> void:
