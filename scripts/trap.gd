182d20641a3949f8db0c0af339465127e1c250cc
Wolfy Hige <castielhige@gmail.com>
2025-07-19 21:18:31 -0400

    trap pause
    trap pause

--- scripts/trap.gd
+++ scripts/trap.gd
@@ -5,23 +5,28 @@ signal trap_timeshift(timeshift_type: String, timescale_new: int)
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var is_active: bool = false
+var is_touching : bool = false
 var is_in_range: bool = false
 var timescale: int = 1
+var player_ref : Player
 
 func _on_body_entered(body: Node2D) -> void:
+	is_touching = true
 	if body is Player && is_active:		
-		var player:Player = body
-		if !player.full_rewind :
-			player.player_rewinded.emit(true)
+		player_ref = body
+		if !player_ref.died:
+			player_ref.player_rewinded.emit(true)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_touching = false
+
 
 func _on_body_near(body: Node2D) -> void:
 	if body is Player :
-		print("is near")
 		is_in_range = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player :
-		print("is not near")
 		is_in_range = false
 
 func _on_trap_timeshift(timeshift_type: String, timescale_new: int) -> void:
@@ -32,6 +37,8 @@ func _process(delta: float) -> void:
 		if is_in_range && !is_active :
 			sprite.play("active")
 			is_active=true
+			if is_touching && player_ref && !player_ref.died:
+				player_ref.player_rewinded.emit(true)
 		elif (is_active && !is_in_range) :
 			sprite.play("default")
 			is_active=false
