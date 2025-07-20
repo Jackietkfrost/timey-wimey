87fad4aa8fb6e318e2a3fdfd4b69078089b5b175
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-07-19 21:43:33 -0400

    changed auto work for points
    Fixed audio player, using it for points

--- scripts/point.gd
+++ scripts/point.gd
@@ -3,16 +3,14 @@ extends Node2D
 @onready var coin_sound: AudioStreamPlayer = $AudioStreamPlayer
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var area_2d: Area2D = $Area2D
-
 func _ready() -> void:
 	sprite.play("default")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		coin_sound.autoplay = true
-		coin_sound.playing = true
-		self.visible = false
+	if body is Player && area_2d.monitoring == true:
 		area_2d.monitoring = false
-		await get_tree().create_timer(coin_sound.stream.get_length(), true, true, true).timeout
+		self.visible = false
+		AudioPlayer.play_FX(preload("res://assets/sounds/sfx/SFX- The Ultimate 2017 16 bit Mini pack/Menu/Ogg/Menu__008.ogg"))
+		await get_tree().create_timer(AudioPlayer.stream.get_length(), true, true, true).timeout
 		self.queue_free()
