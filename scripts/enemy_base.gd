9b95c1e6ef51082ba8a67954974692058f47e179
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-07-20 13:37:09 -0400

    changed enemy

--- scripts/enemy_base.gd
+++ scripts/enemy_base.gd
@@ -1,12 +1,6 @@
-class_name Enemy_Base extends CharacterBody2D
+class_name Enemy_Base extends Path2D
 
 signal timeshift()
 
 @onready var patrol_path: Path2D = $AnimatedSprite2D/Path2D
 @onready var follow_path: PathFollow2D = $Path2D/PathFollow2D
-
-#var curr_point: Vector2 = patrol_path.curve.get
-var curr_point: Vector2 = follow_path.progress
-
-func give_path() -> void:
-	patrol_path.curve.get_point_position(1)
