6e9a646fe646d992702fa01efd12b7cef7d107d9
Wolfy Hige <castielhige@gmail.com>
2025-07-20 12:26:09 -0400

    dsds
    dsdsd

--- scripts/fan.gd
+++ scripts/fan.gd
@@ -5,29 +5,30 @@ signal fan_timeshift(timeshift_type: String, timescale_new: int)
 @onready var gust: AnimatedSprite2D = $AnimatedSprite2D
 @onready var fan: AnimatedSprite2D = $Fan
 
-@export var pushback_strength : Vector2 = Vector2 (70,0)
+@export var fan_strength : float = 70
+@export var pushback_strength : Vector2
 
 var player_ref
 var timescale = 1
 var is_touching = false
-var angle : float
 
 func _on_entered_gust(body: Node2D) -> void:
 	if body is Player:	
 		is_touching = true
 		player_ref = body
 		if (timescale != 0) :
-			player_ref.velocity_modifier += pushback_strength.rotated(angle)
+			player_ref.velocity_modifier += pushback_strength
 
 func _on_exited_gust(body: Node2D) -> void:
 	if body is Player:
 		is_touching = false
 		if (timescale != 0) :
-			player_ref.velocity_modifier -= pushback_strength.rotated(angle)
+			player_ref.velocity_modifier -= pushback_strength
 
 func _ready() :
-	print(self.rotation_degrees)
-	angle = deg_to_rad(rotation_degrees)
+	var angle = deg_to_rad(self.rotation_degrees)
+	pushback_strength = ( Vector2 (fan_strength, 0) ).rotated(angle)
+	
 	gust.play("blow_wind")
 	fan.play("default")
 	
@@ -35,11 +36,11 @@ func _ready() :
 func _on_fan_timeshift(timeshift_type: String, timescale_new: int) -> void:
 	if (timescale_new != 0 && timescale == 0 ) : #game unpaused
 		if (player_ref && is_touching) :
-			player_ref.velocity_modifier += pushback_strength.rotated(angle)
+			player_ref.velocity_modifier += pushback_strength
 		gust.visible = true
 	elif (timescale_new == 0 && timescale != 0): #game paused
 		if (player_ref && is_touching) :
-			player_ref.velocity_modifier -= pushback_strength.rotated(angle)
+			player_ref.velocity_modifier -= pushback_strength
 		gust.visible = false
 	fan.play("default", timescale_new)
 	gust.play("blow_wind", timescale_new)		
